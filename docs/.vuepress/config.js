e202bb2381e996a121cebd9428f91d6f9339f57a
liawnliu <liawnliu@qq.com>
2022-07-11 22:36:26 +0800

    feat: 添加pwa离线程序相关配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,14 @@ module.exports = {
     head: [
         ['meta', {charset: 'UTF-8'}],
         ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ffffff' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     markdown: {
         lineNumbers: true, // 代码块显示行号
@@ -27,6 +35,11 @@ module.exports = {
         '@vuepress/plugin-medium-zoom',
         // yarn add vuepress-plugin-fulltext-search -D。解决全文搜索问题。
         'fulltext-search',
+        // 离线应用
+        ['@vuepress/pwa', {
+            serviceWorker: true, // 用于缓存页面的内容以供离线使用（仅会在生产环境中启用）
+            updatePopup: true, // 站点有内容更新时显一个更新弹框，并提供了一个 refresh 按钮，允许用户立即刷新内容
+        }]
     ],
     // yarn add vuepress-theme-liawn -D。引入自定义主题
     theme: 'vuepress-theme-liawn',
